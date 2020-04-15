8288fb66eb3bd5e56749d7d91d63fad5c707fde1
Andrew Scott <ascott02@gmail.com>
2020-04-15 08:21:06 -0700

    next exp, lr=0.001

--- run_training.sh
+++ run_training.sh
@@ -2,5 +2,5 @@ time python tools/run.py --tasks captioning --datasets youcookII \
     --model butd --config configs/captioning/youcookII/butd.yml \
     --batch_size 64 -nw 32 --clip_gradients True \
     --resume_file content/model_data/butd.pth \
-    -exp youcookII_fine-tune_bs_64_lr_0pt01 \
+    -exp youcookII_fine-tune_bs_64_lr_0pt001 \
     --distributed True --data_parallel True \
